95bb6c301f4f15866869eeaaed5a149ba7eea62a
Jess Manapul <Jess@Jess.home>
2019-02-11 18:19:35 -1000

    completed #4

--- exercises.js
+++ exercises.js
@@ -49,21 +49,21 @@ This function will iterate through the person parameter and console.log the foll
 "President person was a great leader."
 */
 
-var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
+// var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
 
-for (var i = 0; i < presidents.length; i++) {
-   console.log('The value at', i, 'is', presidents[i]);
-}
+// for (var i = 0; i < presidents.length; i++) {
+//    console.log('The value at', i, 'is', presidents[i]);
+// }
 
-console.log('Array Length:', presidents.length);
+// console.log('Array Length:', presidents.length);
 
-function leaders(person) {
-   for (var i = 0; i < person.length; i++) {
-      console.log('President', person[i], 'was a great leader.');
-   }
-}
+// function leaders(person) {
+//    for (var i = 0; i < person.length; i++) {
+//       console.log('President', person[i], 'was a great leader.');
+//    }
+// }
 
-leaders(presidents);
+// leaders(presidents);
 
 
 /* 4) Line Number
@@ -71,6 +71,13 @@ Declare a variable named `stringOfNumbers` and assign its value to an empty stri
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
 
+var stringOfNumbers = '';
+
+for (var i = 10; i < 21; i++){
+   stringOfNumbers += i;
+}
+
+console.log(stringOfNumbers);
 
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array. 
