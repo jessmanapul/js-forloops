0a84a5d7608ced35ab1b30c3689b691139d73c0e
Jess Manapul <Jess@Jess.home>
2019-02-11 18:09:26 -1000

    completed #2

--- exercises.js
+++ exercises.js
@@ -10,9 +10,9 @@ Write a for-loop that will iterate through 20 numbers (starting at 1 and ending 
 */
 
 
-for (var i = 1; i < 21; i++) {
-   console.log('Now serving', i);
-}
+// for (var i = 1; i < 21; i++) {
+//    console.log('Now serving', i);
+// }
 
 /* 2) Pop Charts
 Write a for-loop that will iterate through the topFive array below and console.log the following message:
@@ -25,6 +25,9 @@ This week's chart buster is: '24K Magic.'
 
 var topFive = ["Closer", "Starboy", "I Feel It Coming", "Let Me Love You", "24K Magic"];
 
+for (var i = 0; i < topFive.length; i++) {
+   console.log("This week's chart buster is:", topFive[i]);
+}
 
 /* 3) Dead Presidents
 Declare a variable named `presidents` and assign it to an array containing the following Presidents: Washington, Adams, Jefferson, Madison and Monroe.
